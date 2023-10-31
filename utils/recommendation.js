b7b66847af120cc5c9b081efc56daa35cdac8a4b
ch3lla <chukwumaemmanuella03@gmail.com>
2023-10-31 23:49:48 +0100

    get related artists endpoint

--- utils/recommendation.js
+++ utils/recommendation.js
@@ -1,15 +1,33 @@
 const axios = require('axios');
-const {returnToken} = require('./auth');
-const {returnFavArtists} = require('./getArtist');
 
-let artistIds = returnFavArtists();
-let seed_artists = artistIds.map(artist => artist._id);
-console.log(seed_artists);
-async function getRecommendation(){
-    const response = axios.getAdapter(`
-    https://api.spotify.com/v1/?seed_artists=${seed_artists}`, {
-        headers: {Authorization: `Bearer ${returnToken()}`}
+async function getRecommendation(artistIds, token){
+    /* const response = await axios.get(`
+    https://api.spotify.com/v1/recommendations?limit=1&market=NG&seed_artists=${artistId}&seed_trcaks=${trackId}`, {
+        headers: {Authorization: `Bearer ${token}`}
     })
     const data = response.data;
+    console.log("Recommendation data: ", data); */
     
+    const requests = artistIds.map(artistId => {
+        return axios.get(`https://api.spotify.com/v1/artists/${artistId}/related-artists`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+      });
+      const responses = await Promise.all(requests);
+      const allArtistNames = [];
+      for (const response of responses) {
+        if (response.status === 200) {
+          console.log(`Response for artist ID ${response.request.path}:`, response.data);
+          const artistNames = response.data.artists.map(artist => artist.name);
+            allArtistNames.push(...artistNames);
+        } else {
+          console.error(`Error for artist ID ${response.request.path}:`, response.statusText);
+        }
+      }   
+      console.log("All artist names:", allArtistNames);
+    
+    // const data = response.data;
+    // console.log("recommendation: ", data);
 }
+
+module.exports = getRecommendation;
