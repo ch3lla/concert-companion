1bb77f3aa9293d9fb6c869d740c64c0d1a63363e
ch3lla <chukwumaemmanuella03@gmail.com>
2023-10-31 23:51:21 +0100

    added a few things

--- utils/getArtist.js
+++ utils/getArtist.js
@@ -1,18 +1,24 @@
 const router = require('express').Router();
 const axios = require('axios');
 const {returnToken} = require('./auth');
+const getAudioFeatures = require('./getAudioFeatures');
+const getRecommendation = require('./recommendation');
 
 let artists;
 let mainArtist;
+let token;
+
 router.get('/home', (req, res) => {
     res.render('home');
 })
+
 router.post(`/home`, async (req, res) => {
+    token = await returnToken();
     const performingArtist = req.body.artist;
     res.status(200).send('Request received successfully!');
     try{
         const response = await axios.get(`https://api.spotify.com/v1/search?q=${performingArtist}&type=artist&limit=1&offset=0`, {
-            headers: {Authorization: `Bearer ${returnToken()}`}
+            headers: {Authorization: `Bearer ${token}`}
         });
         const items = await response.data;
         mainArtist = items.artists.items.map(artist => ( {
@@ -22,7 +28,7 @@ router.post(`/home`, async (req, res) => {
         }));
 
         const input = await axios.get(`https://api.spotify.com/v1/me/top/artists?limit=5&offset=0`, {
-            headers: { Authorization: `Bearer ${returnToken()}` }
+            headers: { Authorization: `Bearer ${token}` }
         });
         const data = await input.data;
         artists = data.items.map(artist => ({
@@ -30,57 +36,16 @@ router.post(`/home`, async (req, res) => {
             name: artist.name,
             genres: artist.genres
         }));
+        const artistIds = artists.map(item => item._id);
         console.log(artists);
         console.log(mainArtist);
-        console.log(seed_artists);
-        getTrackData();
-        getAudioFeatures();
+        const artistId = mainArtist[0]._id;
+        const favArtist = artists[0]._id;
+        const recommended_data = getRecommendation(artistIds, token);
+        //const audioFeatures = await getAudioFeatures(artistId, token);
+
     } catch (error){
         console.error(error);
     }
 });
-
-let tracks;
-async function getTrackData(){
-    const artistId = mainArtist[0]._id;
-    const response = await axios.get(`https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=NG`, {
-        headers: { Authorization: `Bearer ${returnToken()}` }
-    });
-    
-    const data = await response.data;
-    tracks = data.tracks.map(track => ({
-        _trackid: track.id,
-        name: track.name
-    }));
-    console.log(tracks);
-}
-
-let audioFeaturesResult;
-async function getAudioFeatures(){
-    try {
-        await getTrackData();
-        if(tracks.length > 0){
-            const _trackid = tracks[0]._trackid;
-            const pArtistAudiofeatures = await axios.get(`https://api.spotify.com/v1/audio-features?ids=${_trackid}`, {
-                headers: {Authorization: `Bearer ${returnToken()}`}
-            });
-            audioFeaturesResult = await pArtistAudiofeatures.data;
-            const danceability = audioFeaturesResult[danceability];
-            const energy = audioFeaturesResult[energy];
-            const tempo = audioFeaturesResult[tempo];
-        
-        } else {
-            console.log('No tracks available to fetch audio features.');
-        }
-    } catch (error){
-        console.error(error);
-    }
-}
-
-function returnFavArtists(){
-    return artists;
-}
-
-
-
-module.exports = {router, returnFavArtists};
+module.exports = router;
