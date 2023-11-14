0a92c8a62dba75452748aad2903979241b6af517
ch3lla <chukwumaemmanuella03@gmail.com>
2023-11-14 08:07:26 +0100

    renamed some variables

--- utils/recommendation.js
+++ utils/recommendation.js
@@ -1,23 +1,22 @@
 const axios = require('axios');
 
 async function getRecommendation(artistIds, token){
-    let artistNames;
+    const relatedArtistNames = [];
     const requests = artistIds.map(artistId => {
         return axios.get(`https://api.spotify.com/v1/artists/${artistId}/related-artists`, {
           headers: { Authorization: `Bearer ${token}` },
         });
       });
       const responses = await Promise.all(requests);
-      const allArtistNames = [];
       for (const response of responses) {
         if (response.status === 200) {
-           artistNames = response.data.artists.map(artist => artist.name);
-            allArtistNames.push(...artistNames);
+           const currentArtistNames = response.data.artists.map(artist => artist.name);
+            relatedArtistNames.push(...currentArtistNames);
         } else {
           console.error(`Error for artist ID ${response.request.path}:`, response.statusText);
         }
       }
-      return allArtistNames;
+      return relatedArtistNames;
 }
 
 module.exports = getRecommendation;
